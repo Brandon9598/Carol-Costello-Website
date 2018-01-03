6d3791a3f4e12ce7eb95a29717e3b10d63129c8d
WRCoders <bjshirts@gmail.com>
2018-01-03 18:30:23 -0500

    Finish making the index page responsive and allow for collapsable navigation

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,30 @@
 $(document).ready(function() {
+     /* Mobile Nav */
+     $('.js--nav-icon').click(function() {
+          var nav = $('.js--main-nav');
+          var icon = $('.js--nav-icon i');
+          
+          nav.slideToggle(200);
+          if(icon.hasClass('ion-navicon-round')){
+               icon.addClass('ion-close-round');
+               icon.removeClass('ion-navicon-round');
+          } else {
+               icon.removeClass('ion-close-round');
+               icon.addClass('ion-navicon-round');
+          }
+     })
+
+     /* TODO: For some reason, waypoints are not working
+     $('.js--section-biography').waypoint(function(direction) {
+          if (direction == "down"){
+               $('nav').addClass('sticky');
+          }
+          else {
+               $('nav').removeClass('sticky');
+          }
+     });
+     */
+     
      // SLIDE SHOW
      var slideIndex = 0;
      showSlides();
